505132caf8196bb02ec87df68da1a5ef74755fdd
Luis Carlin <luiscarlin@gmail.com>
2016-05-31 02:26:56 -0400

    Refactor.

--- babysitter/java-junit/src/main/java/com/luchoc/babysitter/BabySitterCalculator.java
+++ babysitter/java-junit/src/main/java/com/luchoc/babysitter/BabySitterCalculator.java
@@ -1,33 +1,57 @@
 package com.luchoc.babysitter;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BabySitterCalculator {
     
     private static final int START_LIMIT = 17;
     private static final int STOP_LIMIT = 4; 
     private static final int RATE_BEFORE_BED = 12;
+    private static final int OFFSET_FOR_CALCULATIONS = 24;
     
-    public void validateTimes(int startTime, int stopTime) {
+    private int startTime; 
+    private int bedTime; 
+    private int stopTime;
+    
+    public void setTimes(int startTime, int bedTime, int stopTime) { 
         
-        if (! isMilitaryTime(startTime)) { 
-            throw new IllegalArgumentException(startTime + " is not military time");
+        // validate inputs
+        List<Integer> times = Arrays.asList(startTime, bedTime, stopTime);
+        
+        for (int time : times) { 
+            if (!(isMilitaryTime(time))) { 
+                throw new IllegalArgumentException(time + " is not military time");
+            }
         }
         
-        if (! isMilitaryTime(stopTime)) { 
-            throw new IllegalArgumentException(stopTime + " is not military time");
-        }
-        
+        // check limits 
         if (startTime < START_LIMIT && startTime > STOP_LIMIT) { 
-            throw new IllegalArgumentException("Cannot schedule at " + startTime);
+            throw new IllegalArgumentException("Cannot schedule start time at " + startTime);
         }
         
         if (stopTime > STOP_LIMIT && stopTime < START_LIMIT) { 
-            throw new IllegalArgumentException("Cannot schedule at " + stopTime);
+            throw new IllegalArgumentException("Cannot schedule stop time at " + stopTime);
         }
+        
+        // verify order
+        int startTimeWithOffset = startTime + (startTime <= STOP_LIMIT ? OFFSET_FOR_CALCULATIONS : 0);
+        int bedTimeWithOffset = bedTime + (bedTime <= STOP_LIMIT ? OFFSET_FOR_CALCULATIONS : 0); 
+        int stopTimeWithOffset = stopTime + (stopTime <= STOP_LIMIT ? OFFSET_FOR_CALCULATIONS : 0); 
+        
+        if (!(startTimeWithOffset <= stopTimeWithOffset && 
+                startTimeWithOffset <= bedTimeWithOffset &&
+                bedTimeWithOffset <= stopTimeWithOffset)) { 
+            throw new IllegalArgumentException("Order of times should be start time <= bed time <= stop time");
+        }
+        
+        // save inputs
+        this.startTime = startTime;
+        this.bedTime = bedTime;
+        this.stopTime = stopTime;
     }
 
-    public int charge(int startTime, int bedTime, int stopTime) {
-        validateTimes(startTime, stopTime);
-        
+    public int calculate() {
         int charge = 0; 
         
         charge += (bedTime - startTime) * RATE_BEFORE_BED;
--- babysitter/java-junit/src/test/java/com/luchoc/babysitter/BabySitterCalculatorTest.java
+++ babysitter/java-junit/src/test/java/com/luchoc/babysitter/BabySitterCalculatorTest.java
@@ -16,36 +16,32 @@ public class BabySitterCalculatorTest {
     
     @Test(expected=IllegalArgumentException.class)
     public void calculatorThrowsExceptionWhenStartTimeBefore17() {
-        calc.charge(16, 18, 0);
+        calc.setTimes(16, 18, 0);
     }
     
     @Test(expected=IllegalArgumentException.class)
     public void calculatorThrowsExceptionWhenStopTimeAfter4() {
-        calc.charge(18, 0, 5);
+        calc.setTimes(18, 0, 5);
     }
     
     @Test
     public void whenSitterWorksFromStartToBedTheyGet12DollarsPerHour() { 
-        assertEquals(12, calc.charge(18, 19, 19));
-    }
-    
-    @Test
-    public void whenBedtimeIsPastMidnightCharge12DollarsPerHour() { 
-        //assertEquals(12 * 7, calc.charge(17, 1, 1));
+        calc.setTimes(18, 19, 19);
+        assertEquals(12, calc.calculate());
     }
     
     @Test
     public void calculatorDoesNotFailWhenStartTimeAfterMidnight() {
-        calc.charge(2,3,3);
+        calc.setTimes(2,3,3);
     }
     
     @Test
     public void calculatorDoesNotFailWhenStopTimeBeforeMidnight() { 
-        calc.charge(18, 19, 23);
+        calc.setTimes(18, 19, 23);
     }
     
     @Test(expected=IllegalArgumentException.class)
     public void calculatorThrowsExceptionWhenNotUsingMilitaryTime() { 
-        calc.charge(-1,  0,  34); 
+        calc.setTimes(-1,  0,  34); 
     }
 }
